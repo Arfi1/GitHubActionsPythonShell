513115276fed1b6d1ee7a34f9cf4ad9e520878ee
arfi <arma0001@stud.kea.dk>
2024-09-03 18:12:21 +0200

    first commit

---
+++ src/main/java/com/example/githubactionspythonshell/GitHubActionsPythonShellApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionspythonshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsPythonShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsPythonShellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionspythonshell/GitHubActionsPythonShellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionspythonshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsPythonShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
